8b9eada4ca1c9057f62223b3af99b61dc2736bbb
Emil Christiansen <emil.christiansen@ntnu.no>
2022-05-11 21:46:21 +0200

    Updated preprocessing script for templatematching

--- preprocessing.py
+++ preprocessing.py
@@ -22,6 +22,8 @@ from pathlib import Path
 
 from scipy.ndimage import gaussian_filter
 from skimage.exposure import rescale_intensity
+from skimage.morphology import erosion
+from skimage.morphology import disk
 import skimage.filters as skifi
 
 
@@ -31,29 +33,52 @@ def load_pickle(filename):
     return obj
 
 
-def subtract_background_dog(z, sigma_min, sigma_max):
-    blur_max = gaussian_filter(z, sigma_max)
-    blur_min = gaussian_filter(z, sigma_min)
-    return np.maximum(np.where(blur_min > blur_max, z, 0) - blur_max, 0)
+def subtract_background_dog(array, sigma_min, sigma_max):
+    """
+    Subtract the background of a 2D array using difference of gaussians (DOG)
+
+    If the array is not a float array, it will be typecasted into a float array in this function.
+    :param array: The image
+    :param sigma_min: The lower sigma
+    :param sigma_max: The upper sigma
+    :return:
+    """
+    if not array.dtype == float:
+        array = array.astype(float)
+    blur_max = gaussian_filter(array, sigma_max)
+    blur_min = gaussian_filter(array, sigma_min)
+    return np.maximum(np.where(blur_min > blur_max, array, 0) - blur_max, 0)
 
 
 def process_image(image, map_min_to_zero=True, background_subtract_function=None, blur_sigma=None, min_intensity=None,
-                  gamma_value=None, rescale=True, stripes=False, sigma_min=3, sigma_max=8):
-    if stripes:
-        # remove the vertical stripes artifact by subtracting the median of each column
-        median_cols = np.median(image, axis=0)
-        image = image - median_cols
+                  gamma_value=None, rescale=True, erode=False, footprint=6, rescale_range='dtype', **kwargs):
+    """
+    Process an image and prepare it for templatematching
+
+    :param image: The image to prepare
+    :param map_min_to_zero: Whether to map the minimum intensity to zero (1st step)
+    :param background_subtract_function: Whether to perform a background subtraction (2nd step)
+    :param blur_sigma: The sigma to blur the image with (3rd step)
+    :param min_intensity: The minimum intensity to keep after the three first steps (4th step). Intensities lower than this will be set to zero
+    :param gamma_value: The gamma correction value (5th step)
+    :param erode: Whether to erode the image after the 5th step (6th step)
+    :param footprint: The erosion footprint
+    :param rescale: Whether to rescale the image after the 6th step.
+    :param rescale_range: The range of the output array. Default is to stretch it between the image dtype bounds.
+    :param kwargs: Optional keyword arguments passed to the background subtract function.
+    :return:
+    """
 
     if map_min_to_zero:
-        # map the minimum to 0
+        # Subtract minimum intensity
         image = image - image.min()
 
     if background_subtract_function is not None:
-        # remove the entire diffuse background
-        image = background_subtract_function(image, sigma_min, sigma_max)
+        # Remove background
+        image = background_subtract_function(image, **kwargs)
 
     if blur_sigma is not None:
-        # smooth the image
+        # blur the image
         image = skifi.gaussian(image, sigma=blur_sigma)
 
     if min_intensity is not None:
@@ -64,9 +89,14 @@ def process_image(image, map_min_to_zero=True, background_subtract_function=None
         # change the gamma of the images
         image = image ** gamma_value
 
+    if erode:
+        # erode the image
+        image = erosion(image, disk(footprint))
+
     if rescale:
-        # remap intensities to 0-1 range
-        image = rescale_intensity(image)
+        # remap intensities to different range
+        image = rescale_intensity(image, in_range='image', out_range=rescale_range)
+
     return image
 
 
@@ -83,12 +113,16 @@ if __name__ == '__main__':
                         help='Lower intensity threshold. Intensities below this value will be set to zero.')
     parser.add_argument('--gamma', type=float, help='Gamma value used to gamma-scale the images')
     parser.add_argument('--rescale', action='store_true',
-                        help='Whether to remap intensities to [0, 1] after preprocessing')
+                        help='Whether to rescale intensities after preprocessing')
+    parser.add_argument('--rescale_range', type=float, nargs=2, default=[0, 1], help='The output intensity range')
+    parser.add_argument('--erode', action='store_true', help='Whether to erode the data using a footprint or not')
+    parser.add_argument('--footprint', type=int, default=6, help='The erosion footprint')
     parser.add_argument('--stripes', action='store_true',
                         help='Whether to remove vertical stripes in the diffraction pattern.')
     parser.add_argument('--lazy', action='store_true', help='Whether to work on a lazy signal or not')
     parser.add_argument('-v', '--verbose', dest='verbosity', default=0, action='count', help='Set verbose level')
-    parser.add_argument('--no_overwrite', action='store_false', help='Whether to not overwrite any existing preprocessed data')
+    parser.add_argument('--no_overwrite', action='store_false',
+                        help='Whether to not overwrite any existing preprocessed data')
 
     arguments = parser.parse_args()
 
@@ -112,9 +146,11 @@ if __name__ == '__main__':
         'min_intensity': arguments.min_intensity,
         'gamma_value': arguments.gamma,
         'rescale': arguments.rescale,
-        'stripes': arguments.stripes,
+        'rescale_range': arguments.rescale_range,
+        'erode': arguments.erode,
+        'footprint': arguments.footprint,
         'sigma_min': arguments.min_sigma,
-        'sigma_max': arguments.max_sigma
+        'sigma_max': arguments.max_sigma,
     }
 
     table = tabulate([[key, preprocessing_kwargs[key]] for key in preprocessing_kwargs], headers=['Parameter', 'Value'])
@@ -127,7 +163,7 @@ if __name__ == '__main__':
     preprocessed_signal.change_dtype(np.float32)
     logger.info(f'Running preprocessing on {signal}')
     preprocessed_signal.map(process_image, **preprocessing_kwargs)
-    logger.info(f'Added preprocessing')
+    logger.info(f'Finished preprocessing')
 
     output_path = input_name.with_name(f'{input_name.stem}_preprocessed{input_name.suffix}')
     logger.info(f'Saving signal to {output_path.absolute()}')
