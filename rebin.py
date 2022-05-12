b5bdbaf9d860ba455404d768cc33e8e98f49161c
Emil Christiansen <emil.christiansen@ntnu.no>
2022-05-12 12:06:09 +0200

    Changed logging in rebin script

--- rebin.py
+++ rebin.py
@@ -1,12 +1,17 @@
 import logging
-#basic config must be done before loading other packages
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s - %(message)s', datefmt='%d-%m-%Y %H:%M:%S')
+
+logger = logging.getLogger(__file__)
+logger.propagate = False
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s', datefmt='%d-%m-%Y %H:%M:%S')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
 import argparse
 from pathlib import Path
 import hyperspy.api as hs
-import pyxem as pxm
-import numpy as np
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -20,19 +25,23 @@ if __name__ == '__main__':
     arguments = parser.parse_args()
 
     log_level = [logging.WARNING, logging.INFO, logging.DEBUG][min([arguments.verbosity, 2])]
-    logging.root.setLevel(log_level)
+    logger.setLevel(log_level)
 
     args_as_str = [f'\n\t{arg!r} = {getattr(arguments, arg)}' for arg in vars(arguments)]
-    logging.debug(f'Running rebinning script with arguments:{"".join(args_as_str)}')
+    logger.debug(f'Running rebinning script with arguments:{"".join(args_as_str)}')
 
     # Load signal
     input_name = Path(arguments.filename)
-    logging.info(f'Loading "{input_name.absolute()}" with lazy={arguments.lazy}')
+    logger.info(f'Loading "{input_name.absolute()}" with lazy={arguments.lazy}')
     signal = hs.load(input_name, lazy=arguments.lazy)
 
-    #Rebin signal
+    # Rebin signal
+    logger.info(f'Rebinning signal using scale {arguments.scale}')
     rebinned_signal = signal.rebin(scale=arguments.scale)
 
     scales_as_string = '_'.join([f'{scale:.0f}' for scale in arguments.scale])
     output_name = input_name.with_name(f'{input_name.stem}_rebinned_{scales_as_string}{input_name.suffix}')
+    logger.info(f'Saving signal to "{output_name.absolute()}"')
     rebinned_signal.save(output_name)
+
+    logger.info('Finished')
